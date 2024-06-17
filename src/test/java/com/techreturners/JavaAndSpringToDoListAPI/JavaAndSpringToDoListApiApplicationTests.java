6b2059a01af6cee228285a81b1276c0bafc3b5cd
Smitha <smitha.kotian@gmail.com>
2024-06-17 23:26:44 +0100

    feat: add initial project set up for ToDoList API

---
+++ src/test/java/com/techreturners/JavaAndSpringToDoListAPI/JavaAndSpringToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.JavaAndSpringToDoListAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAndSpringToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
