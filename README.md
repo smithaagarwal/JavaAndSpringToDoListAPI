5c6b4c8719f7cf242c62144be37515988930cb03
Smitha <smitha.kotian@gmail.com>
2024-06-19 18:16:34 +0100

    refactor: enable integration testing and local debugging by segregating local and docker Spring profiles.

---
+++ README.md
@@ -0,0 +1,20 @@
+_How to run locally_
+
+Open a terminal and cd into project directory
+
+- `mvn clean package`
+- java -jar  .\target\JavaAndSpringToDoListAPI-1.0.0.jar --spring.profiles.active=local`
+
+
+_How to run via Docker_
+
+Open a terminal and cd into project directory
+
+- `mvn clean package`
+- `docker-compose up --build`
+
+_PS : While running from within the Docker container, Spring reads the DB connection and user details from the environment variable set in the `docker-compose.yml`_ 
+
+
+_How to debug in Intellij community edition_
+![Debugging](intellij-debug.png "IntelliJ Debugging")
--- src/test/java/com/techreturners/JavaAndSpringToDoListAPI/ToDoListApiApplicationTests.java
+++ src/test/java/com/techreturners/JavaAndSpringToDoListAPI/ToDoListApiApplicationTests.java
@@ -4,46 +4,46 @@ import com.techreturners.JavaAndSpringToDoListAPI.domain.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// TODO: With Docker, we need to use TestContainers for Integration Test
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local")
 class ToDoListApiApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
-//    @Autowired
-//    private WebTestClient webTestClient;
-//
-//    @Test
-//    void whenPostRequestThenTaskCreated() {
-//        var task = Task.of("Gardening", false);
-//
-//        webTestClient.post().uri("/tasks").bodyValue(task).exchange().expectStatus().isCreated().expectBody(Task.class).value(actualTask -> {
-//            assertThat(actualTask).isNotNull();
-//            assertThat(actualTask.description()).isEqualTo(task.description());
-//        });
-//        webTestClient.get().uri("/tasks").exchange().expectStatus().isOk().expectBodyList(Task.class).hasSize(1);
-//
-//    }
-//
-//    @Test
-//    void whenGetRequestBeforeAddingTasksThenShowListSizeZero() {
-//        webTestClient.get().uri("/tasks").exchange().expectStatus().isOk().expectBodyList(Task.class).hasSize(0);
-//    }
-//
-//    @Test
-//    void whenPostWithInvalidDescriptionThenShowError() {
-//        var actualTask = Task.of("Ga", false);
-//
-//        webTestClient.post().uri("/tasks")
-//                .bodyValue(actualTask)
-//                .exchange()
-//                .expectStatus().isBadRequest()
-//                .expectBody(Task.class)
-//                .value(response -> {
-//                    assertThat(response.description())
-//                            .isEqualTo("The task should at least be 3 characters long");
-//                });
-//    }
+    @Test
+    void whenPostRequestThenTaskCreated() {
+        var task = Task.of("Gardening", false);
+
+        webTestClient.post().uri("/tasks").bodyValue(task).exchange().expectStatus().isCreated().expectBody(Task.class).value(actualTask -> {
+            assertThat(actualTask).isNotNull();
+            assertThat(actualTask.description()).isEqualTo(task.description());
+        });
+        webTestClient.get().uri("/tasks").exchange().expectStatus().isOk().expectBodyList(Task.class).hasSize(1);
+
+    }
+
+    @Test
+    void whenGetRequestBeforeAddingTasksThenShowListSizeZero() {
+        webTestClient.get().uri("/tasks").exchange().expectStatus().isOk().expectBodyList(Task.class).hasSize(0);
+    }
+
+    @Test
+    void whenPostWithInvalidDescriptionThenShowError() {
+        var actualTask = Task.of("Ga", false);
+
+        webTestClient.post().uri("/tasks")
+                .bodyValue(actualTask)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(Task.class)
+                .value(response -> {
+                    assertThat(response.description())
+                            .isEqualTo("The task should at least be 3 characters long");
+                });
+    }
 }
