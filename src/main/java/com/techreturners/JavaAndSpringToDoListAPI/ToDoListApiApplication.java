053417a20138b1d57f9bf95c801ab5cf038761d5
Smitha <smitha.kotian@gmail.com>
2024-06-20 22:38:01 +0100

    feat: add the CORS configuration to enable the connection from a webclient hosted on a different domain

--- src/main/java/com/techreturners/JavaAndSpringToDoListAPI/ToDoListApiApplication.java
+++ src/main/java/com/techreturners/JavaAndSpringToDoListAPI/ToDoListApiApplication.java
@@ -2,6 +2,9 @@ package com.techreturners.JavaAndSpringToDoListAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ToDoListApiApplication {
@@ -10,4 +13,15 @@ public class ToDoListApiApplication {
         SpringApplication.run(ToDoListApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "DELETE", "POST");
+            }
+        };
+    }
 }
